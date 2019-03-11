b7bdf6ea4743e2cddafa9ccad7e9e049f2cb7bb9
Iryna Kanivets <irynakanivets@gmail.com>
2019-03-11 14:24:17 +0200

    Added routes

--- index.js
+++ index.js
@@ -1,71 +1,38 @@
-const Joi = require('joi');
+const startupDebugger = require('debug')('app:startup');
+const dbDebugger = require('debug')('app:db');
+const morgan = require('morgan');
+const config = require('config');
+const helmet = require('helmet');
 const express = require('express');
+const logger = require('./middleware/logger'); 
+const courses = require('./routes/courses');
+const home = require('./routes/home');
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', './views');
+
 app.use(express.json());
-
-const courses = [
-    {id: 1, name: 'course1'},
-    {id: 2, name: 'course2'},
-    {id: 3, name: 'course3'},
-];
-
-app.get('/', (req, res) => {
-    res.send('Hello world!');
-})
-
-app.get('/api/courses', (req, res) => {
-    res.send(courses);
-})
-
-app.get('/api/courses/:id', (req, res) => {
-  const course =  courses.find(c => c.id === parseInt(req.params.id));
-  if(!course) return res.status(404).send('The course with given ID was not found')
-  res.send(course)
-})
-
-app.post('/api/courses', (req, res) => { 
- 
-    const { error } = validateCourse(req.body);
-    if(error) return res.status(400).send(error.details[0].message);
-
-    const course = {
-        id: courses.length + 1,
-        name: req.body.name
-    }
-    courses.push(course);
-    res.send(course);
-}); 
-
-app.put('/api/courses/:id', (req, res) => {
-    const course =  courses.find(c => c.id === parseInt(req.params.id));
-    if(!course) return res.status(404).send('The course with given ID was not found');
-    
-    const { error } = validateCourse(req.body);
-    if(error) return res.status(400).send(error.details[0].message);
-
-    course.name = req.body.name;
-    res.send(course);
-});
-
-app.delete('/api/courses/:id', (req, res) => {
-    const course =  courses.find(c => c.id === parseInt(req.params.id));
-    if(!course) return res.status(404).send('The course with given ID was not found')
-    
-    const index = courses.indexOf(course);
-    courses.splice(index, 1);
-
-    res.send(course);
-});
+app.use(express.urlencoded({ extended: true})); // key=value&key=value  
+app.use(express.static('public'));
+app.use(helmet());
+app.use(morgan('tiny'));
+app.use('/api/courses', courses);
+app.use('/', home);
 
 
-function validateCourse(course){
-    const schema = {
-        name: Joi.string().min(3).required()
-    }
+// Configuration
+console.log('application name ' + config.get('name'));
+console.log('Mail server ' + config.get('mail.host'));
 
-    return Joi.validate(course, schema);
+// custom middelware function
+if(app.get('env') === 'development'){
+    app.use(logger);
 }
+
+// Do work...
+//dbDebugger('Connected to the database...')
+
 
 // PORT
 const port = process.env.PORT || 3000
